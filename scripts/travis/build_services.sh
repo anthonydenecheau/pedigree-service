273f790732f4f5204ec88f86ddabd9eb53418100
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-03-25 17:47:38 +0100

    :construction: build image docker

--- scripts/travis/build_services.sh
+++ scripts/travis/build_services.sh
@@ -1,3 +1,3 @@
 echo "Building with travis commit of $BUILD_NAME ..."
 mvn clean package -Dquarkus.container-image.build=true
-docker build -t anthonydenecheau/scc-pedigree-service -f ./src/main/docker/Dockerfile.native .
+docker build -t anthonydenecheau/scc-pedigree-service:$BUILD_NAME -f ./src/main/docker/Dockerfile.native .
