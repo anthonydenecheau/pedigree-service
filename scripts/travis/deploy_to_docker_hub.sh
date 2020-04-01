2186009e5287d9c31458bcad6904c2ccc2e80d5a
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-04-01 16:16:45 +0200

    :construction: travis login

--- scripts/travis/deploy_to_docker_hub.sh
+++ scripts/travis/deploy_to_docker_hub.sh
@@ -1,3 +1,3 @@
 echo "Pushing service docker images to docker hub ...."
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+travis login --github-token $DOCKER_TOKEN
 docker push anthonydenecheau/scc-pedigree-service:$BUILD_NAME
