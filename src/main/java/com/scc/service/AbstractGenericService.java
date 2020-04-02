c18c5833ce30eeb1533e3913cc18cbab7ec72e94
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-04-02 11:30:48 +0200

    :contruction: build image

--- src/main/java/com/scc/service/AbstractGenericService.java
+++ src/main/java/com/scc/service/AbstractGenericService.java
@@ -11,39 +11,5 @@ public abstract class AbstractGenericService<T> {
     protected void setType(Class<T> type) {
         this.type = type;
     }
-
-    
-    /**
-     * Le rôle détermine l'affichage de l'objet lors de la serialization
-     * Le rôle est déclaré par le biais d'une annotation sur l'objet
-     * @return
-     
-    public boolean hasRole() {
-
-        //System.out.println("Start hasRole");
-        boolean result = false;
-
-        try {
-
-            String _role = null;
-            if (getType().isAnnotationPresent(Role.class)) {
-                _role = getType().getAnnotation(Role.class).name();
-            }
-            if ("".equals(_role) || _role == null) {
-                return false;
-            }
-            if (identity.hasRole(_role))
-                result = true;
-            else
-                result = false;
-
-        } catch (Exception e) {
-            //System.out.println("Error hasRole" + e.getMessage());
-        } finally {
-            //System.out.println("End hasRole");
-        }
-        return result;
-    }
-    */
     
 }
