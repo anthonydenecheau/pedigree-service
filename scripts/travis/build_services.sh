b6e3574455693e172ea622161baca230d725db20
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-04-15 10:29:19 +0200

    :construction: build timeout

--- scripts/travis/build_services.sh
+++ scripts/travis/build_services.sh
@@ -1,3 +1,3 @@
 echo "Building with travis commit of $BUILD_NAME ..."
-./mvnw package -Pnative -Dnative-image.docker-build=true -Dquarkus.profile=docker-profile
+travis_wait 30 ./mvnw package -Pnative -Dnative-image.docker-build=true -Dquarkus.profile=docker-profile
 docker build -f src/main/docker/Dockerfile.native -t anthonydenecheau/scc-pedigree-service:$BUILD_NAME .
