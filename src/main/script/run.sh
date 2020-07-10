1639ac9cd821a06152c3a5db58c106d837570250
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-07-10 13:14:20 +0200

    :construction_worker:

--- src/main/script/run.sh
+++ src/main/script/run.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/sh
 
 ## Start the proxy
@@ -12,18 +11,3 @@ sleep 10
 ## Start the server
 echo "Start the server within PROFILE: " $PROFILE
 ./application -Dquarkus-profile=$PROFILE -Dquarkus.http.host=0.0.0.0 -Dquarkus.http.port=$PORT -Djavax.net.ssl.trustStore=/work/cacerts
-=======
-#!/bin/sh
-
-## Start the proxy
-# pending fix : cf. https://github.com/quarkusio/quarkus/issues/9985
-echo "Start the proxy"
-/usr/local/bin/cloud_sql_proxy -instances=${DATASOURCE_URL}=tcp:5432 -credential_file=ws-openid.json &
-
-# wait for the proxy to spin up
-sleep 10
-
-## Start the server
-echo "Start the server within PROFILE: " $PROFILE
-./application -Dquarkus-profile=$PROFILE -Dquarkus.http.host=0.0.0.0 -Dquarkus.http.port=$PORT -Djavax.net.ssl.trustStore=/work/cacerts
->>>>>>> refs/remotes/origin/dev
