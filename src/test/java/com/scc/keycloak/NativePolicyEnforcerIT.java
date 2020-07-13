8e0f9c1786648c3fdad219bdae61e847f4a7b7f3
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-07-13 17:08:35 +0200

    :construction_worker: disable test

--- src/test/java/com/scc/keycloak/NativePolicyEnforcerIT.java
+++ src/test/java/com/scc/keycloak/NativePolicyEnforcerIT.java
@@ -1,10 +1,12 @@
 package com.scc.keycloak;
 
+import io.quarkus.test.junit.DisabledOnNativeImage;
 import io.quarkus.test.junit.NativeImageTest;
 
 /**
  * @author <a href="mailto:psilva@redhat.com">Pedro Igor</a>
  */
-@NativeImageTest
+//@NativeImageTest
+@DisabledOnNativeImage
 public class NativePolicyEnforcerIT extends PolicyEnforcerTest {
 }
