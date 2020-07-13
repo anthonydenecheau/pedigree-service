f299b910a2068dbec7ec9facaa63404ac97a9097
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-07-13 16:25:11 +0200

    :construction_worker: run test

--- src/main/java/com/scc/exception/NotFoundExceptionMapper.java
+++ src/main/java/com/scc/exception/NotFoundExceptionMapper.java
@@ -1,10 +1,13 @@
 package com.scc.exception;
 
+import javax.annotation.Priority;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import javax.ws.rs.Priorities;
 
 @Provider
+@Priority(Priorities.USER + 1)
 public class NotFoundExceptionMapper implements ExceptionMapper<NotFoundException> {
     
     @Override
