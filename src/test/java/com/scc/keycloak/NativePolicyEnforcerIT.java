b1ed929be3ca432f3dfb5aaee5895132a8f79d78
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-12-24 16:38:06 +0100

    :rocket: up to 1.10.5.Final

--- src/test/java/com/scc/keycloak/NativePolicyEnforcerIT.java
+++ src/test/java/com/scc/keycloak/NativePolicyEnforcerIT.java
@@ -6,5 +6,5 @@ import io.quarkus.test.junit.NativeImageTest;
  * @author <a href="mailto:psilva@redhat.com">Pedro Igor</a>
  */
 @NativeImageTest
-public class NativePolicyEnforcerIT extends PolicyEnforcerTest {
+public class NativePolicyEnforcerIT {
 }
