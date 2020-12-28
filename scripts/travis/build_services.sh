b8bad2f4c388b3a9adccea5deeeffa72dacee739
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-12-28 09:44:50 +0100

    :construction: build again

--- scripts/travis/build_services.sh
+++ scripts/travis/build_services.sh
@@ -1,2 +1,2 @@
 echo "Building with travis commit of $BUILD_NAME ..."
-./mvnw package -Pnative -Dnative-image.docker-build=true -Dquarkus.profile=local
+#./mvnw package -Pnative -Dnative-image.docker-build=true -Dquarkus.profile=local
--- scripts/travis/tag_build.sh
+++ scripts/travis/tag_build.sh
@@ -1,7 +1,6 @@
 echo "Tagging build with $BUILD_NAME"
 export TARGET_URL="https://api.github.com/repos/anthonydenecheau/pedigree-service/releases?access_token=$GITHUB_TOKEN"
 
-
 body="{
   \"tag_name\": \"$BUILD_NAME\",
   \"target_commitish\": \"master\",
@@ -15,3 +14,5 @@ curl -k -X POST \
   -H "Content-Type: application/json" \
   -d "$body" \
   $TARGET_URL
+
+  
