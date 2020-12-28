4308260a9721f92e0a35757e251541a39aff60ea
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-12-28 18:39:49 +0100

    :construction: tag and push

--- scripts/travis/deploy_to_docker_hub.sh
+++ scripts/travis/deploy_to_docker_hub.sh
@@ -2,5 +2,4 @@ echo "Pushing service docker images to docker hub ...."
 #docker logout
 #docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 #docker push anthonydenecheau/scc-pedigree-service:$BUILD_NAME
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin 
-     && docker push anthonydenecheau/scc-pedigree-service:$BUILD_NAME
+echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin && docker push anthonydenecheau/scc-pedigree-service:$BUILD_NAME
