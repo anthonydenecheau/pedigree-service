c8f5acedf67532cceff8169721053588b0752b2d
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-12-28 16:32:01 +0100

    :construction: debug tag and push

--- scripts/travis/tag_build.sh
+++ scripts/travis/tag_build.sh
@@ -10,7 +10,8 @@ body="{
   \"prerelease\": true
 }"
 
-echo "### body ###"
-echo $body
-
-curl -k -X POST -H "Content-Type: application/json" -H "Authorization: token $GITHUB_TOKEN" -d "$body" $TARGET_URL
+curl -k -X POST \
+ -H "Content-Type: application/json" \
+ -H "Authorization: token $GITHUB_TOKEN" \
+ -d "{\"tag_name\": \"$BUILD_NAME\",\"target_commitish\": \"master\",\"name\": \"$BUILD_NAME\",\"body\": \"Release of version $BUILD_NAME\",\"draft\": true,\"prerelease\": true}" \
+ $TARGET_URL
