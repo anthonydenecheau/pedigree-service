d2d05d6b1a53be06d5184c1735dd4539e96a0eb5
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2020-12-28 09:35:15 +0100

    :construction: build again

--- scripts/travis/build_services.sh
+++ scripts/travis/build_services.sh
@@ -1,2 +1,2 @@
 echo "Building with travis commit of $BUILD_NAME ..."
-travis_wait 15 ./mvnw package -Pnative -Dnative-image.docker-build=true -Dquarkus.profile=local
+./mvnw package -Pnative -Dnative-image.docker-build=true -Dquarkus.profile=local
